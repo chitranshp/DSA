ae46bcc42015392a0f8324741e4090a737e16b93
chitranshp <chitransh37@gmail.com>
2022-09-02 22:14:44 +0530

    Update file

---
+++ Linked list/MoveLastToFront.cpp
@@ -0,0 +1,69 @@
+//https://practice.geeksforgeeks.org/problems/move-last-element-to-front-of-a-linked-list/1?page=5&category[]=Linked%20List&sortBy=submissions
+
+#include <bits/stdc++.h>
+using namespace std;
+
+class ListNode
+{
+public:
+    int val;
+    ListNode *next;
+    ListNode(int x)
+    {
+        val=x;
+        next=NULL;
+    }
+};
+
+class Solution{
+public:
+    ListNode *moveToFront(ListNode *head)
+    {
+        ListNode *temp = head, *newHead;
+        if(head == NULL || head->next == NULL)
+            return head;
+            
+        while(temp->next->next != NULL)
+            temp = temp->next;
+        
+        newHead = temp->next;
+        temp->next = NULL;
+        newHead->next = head;
+        return newHead;
+    }
+};
+
+
+int main()
+{
+    int t;
+    cin>>t;
+    while(t--)
+    {
+        int n;
+        cin>>n;
+        vector<ListNode*> a(n);
+        for(int i=0;i<n;i++)
+        {
+            int x;
+            cin>>x;
+            a[i]=new ListNode(x);
+            if(i!=0)
+                a[i-1]->next=a[i];
+        }
+        ListNode *head=a[0];
+        Solution ob;
+        head=ob.moveToFront(head);
+        while(head!=NULL)
+        {
+            cout<<head->val;
+            if(head->next!=NULL)
+            {
+                cout<<" ";
+            }
+            head=head->next;
+        }
+        cout<<endl;
+    }
+    return 0;
+}
