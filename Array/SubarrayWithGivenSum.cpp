f2bc9e97071dfc001554260c92ea1a6fd276a934
chitranshp <chitransh37@gmail.com>
2022-09-11 00:37:07 +0530

    Update file

--- Array/SubarrayWithGivenSum.cpp
+++ Array/SubarrayWithGivenSum.cpp
@@ -9,7 +9,8 @@ class Solution
     vector<int> subarraySum(int arr[], int n, long long s)
     {
         int current_sum = arr[0], beg = 0, end, i;
-        for(i = 1; i < n; i++)
+                int current_sum = arr[0], beg = 0, i;
+        for(i = 1; i <= n; i++)
         {
             while(current_sum > s && beg < i - 1)
             {
@@ -19,8 +20,8 @@ class Solution
             
             if(current_sum == s)
                 return {beg +  1, i};
-            
-            current_sum += arr[i];
+            if(i < n)                       //Since, i <= n
+                current_sum += arr[i];      //After the addition, it will check for the sum in next iteration. Therefore, i <= n in loop condition to include the last element also.
         }
         return {-1};
     }
