5dd9929743002a017a45df661d1e0e3e255cbec7
chitranshp <chitransh37@gmail.com>
2022-09-24 12:02:45 +0530

    Update file

---
+++ Misc/PrintAllFilesAndSubFolders.cpp
@@ -0,0 +1,16 @@
+#include <string>
+#include <iostream>
+#include <filesystem>
+namespace fs = std::filesystem;
+
+int main() {
+    std::string path = "D:/Updated Repo DSA/DSA/";          //Path
+    for (const auto & entry : fs::directory_iterator(path))
+        std::cout << entry.path() << std::endl;
+
+    std::string path1 = "D:/Updated Repo DSA/DSA/";         //Path
+    for (const auto & entry : fs::recursive_directory_iterator(path))
+        std::cout << entry.path() << std::endl;
+
+    return 0;
+}
