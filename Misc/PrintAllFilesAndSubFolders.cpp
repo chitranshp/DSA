a8d1dfc3e3d95d3466e15454b016967a8869f11d
chitranshp <chitransh37@gmail.com>
2022-09-24 12:06:48 +0530

    Add comment

--- Misc/PrintAllFilesAndSubFolders.cpp
+++ Misc/PrintAllFilesAndSubFolders.cpp
@@ -6,7 +6,7 @@ namespace fs = std::filesystem;
 int main() {
     std::string path = "D:/Updated Repo DSA/DSA/";          //Path
     for (const auto & entry : fs::directory_iterator(path))
-        std::cout << entry.path() << std::endl;
+        std::cout << entry.path().string() << std::endl;    //Appended .string() when passing it to cout to make an explicit convesrion to string
 
     std::string path1 = "D:/Updated Repo DSA/DSA/";         //Path
     for (const auto & entry : fs::recursive_directory_iterator(path))
