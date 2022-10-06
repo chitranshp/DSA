e2455b540e77c6eda3e0a171744de27988c150b1
chitranshp <chitransh37@gmail.com>
2022-10-06 23:55:09 +0530

    Add file

---
+++ Misc/MaximizeSum.cpp
@@ -0,0 +1,17 @@
+// https://practice.geeksforgeeks.org/problems/maximize-arrii-of-an-array0026/1 
+    
+public:
+    int Maximize(int a[],int n)
+    {
+        unsigned int res = 0;
+        sort(a, a + n);
+        int mod = pow(10, 9) + 7;
+        for(int i = 0; i < n; i++)
+        {
+            res = res + a[i] * i;
+            res = res % mod;
+        }
+        
+        return res;
+    }
+};
