60815db176e6df216864c28b7c23b81a81e9c09d
chitranshp <chitransh37@gmail.com>
2022-10-09 02:51:17 +0530

    Add file

---
+++ Recursion/ReverseString.cpp
@@ -0,0 +1,32 @@
+#include<iostream>
+#include<string>
+#include<algorithm>
+
+using namespace std;
+
+void reverseString(string &s,int p, int q)
+{
+    if(p >= q)
+        return;
+    
+    swap(s[p++], s[q--]);
+    reverseString(s, p, q);
+
+    /*
+    string dummy = ""; 
+    if(index >= s.length())
+        return;
+    reverseString(s,index + 1, dummy);
+
+    dummy.push_back(s[index]);
+
+    */
+}
+
+int main()
+{
+    string s = "Hello";
+    reverseString(s, 0, s.length() - 1);
+    cout<<s;
+    return 0;
+}
