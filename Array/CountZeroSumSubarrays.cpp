55e0de9f08182396c98a10ba83e1e51f04ea0c7d
chitranshp <chitransh37@gmail.com>
2022-10-08 18:23:47 +0530

    Add File

---
+++ Array/CountZeroSumSubarrays.cpp
@@ -0,0 +1,24 @@
+//https://practice.geeksforgeeks.org/problems/zero-sum-subarrays1825/1?page=2&curated[]=1&sortBy=submissions
+
+class Solution{
+    public:
+    //Function to count subarrays with sum equal to 0.
+    ll findSubarray(vector<ll> arr, int n ) 
+    {
+        ll currsum = 0;
+        int i, count = 0;
+        unordered_map<ll, int> prefixsum;
+        
+        for(i = 0; i < n; i++)
+        {
+            currsum += arr[i];
+            
+            if(currsum == 0)
+                count++;
+            if(prefixsum.find(currsum) != prefixsum.end())
+                count = count + prefixsum[currsum];
+            prefixsum[currsum]++;
+        }
+        return count;
+    }
+};
