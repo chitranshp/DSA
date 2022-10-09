611e41a663effd2a155360da4d3f24361f21c681
chitranshp <chitransh37@gmail.com>
2022-10-09 16:10:45 +0530

    Add file

---
+++ Recursion/AddStrings.cpp
@@ -0,0 +1,36 @@
+//https://leetcode.com/problems/add-strings/submissions/
+
+class Solution {
+public:
+    void addHelper(string &num1, string &num2, int p, int q, int carry, string &ans)
+    {
+        if(p < 0 && q < 0 && carry == 0)
+        {
+            return;
+        }
+        
+        int first = 0;
+        int second = 0;
+        int csum = 0;
+        
+        if(p >= 0)
+            first = num1[p] - '0';
+        if(q >= 0)
+            second = num2[q] - '0';
+        
+        csum = first + second + carry;
+        carry = csum/10;
+        csum = csum % 10;
+        ans.push_back(csum + '0');
+        
+        addHelper(num1, num2, p - 1, q - 1, carry, ans);
+        
+    }
+    string addStrings(string num1, string num2) 
+    {
+        string res;
+        addHelper(num1, num2, num1.length() - 1, num2.length() - 1, 0, res);
+        reverse(res.begin(), res.end());
+        return res;
+    }
+};
