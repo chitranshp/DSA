63fded6cbd5880eed640a971c77df121a125cc38
chitranshp <chitransh37@gmail.com>
2022-10-30 22:28:45 +0530

    Create ClimbingStairs.cpp

---
+++ DP/ClimbingStairs.cpp
@@ -0,0 +1,27 @@
+// https://leetcode.com/problems/climbing-stairs/
+
+class Solution {
+public:
+    int climbStairs(int n) 
+    {
+        if(n == 0)
+            return 0;
+        if(n == 1)
+            return 1;
+        if(n == 2)
+            return 2;
+        
+        int before2step = 1;        //When there is only 1 step
+        int before1step = 2;        //When there are two steps.
+        int totalways = before2step + before1step;      //When there are three steps
+        
+        for(int i = 2; i < n; i++)
+        {
+            totalways = before2step + before1step;
+            before2step = before1step;
+            before1step = totalways;
+        }
+        
+        return totalways;
+    }
+};
