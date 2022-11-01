adfa97a72a626fc72143ceffa279308756e2e9bd
chitranshp <chitransh37@gmail.com>
2022-11-02 00:03:12 +0530

    Updated code

--- Array/SingleElementInSortedArray.cpp
+++ Array/SingleElementInSortedArray.cpp
@@ -1,1 +1,28 @@
 // https://leetcode.com/problems/single-element-in-a-sorted-array/
+
+//  For odd index x, x ^ 1 = x - 1
+//  For even index x, x ^ 1 = x + 1
+
+
+class Solution {
+public:
+    int singleNonDuplicate(vector<int>& nums) 
+    {
+        int mid = nums.size()/2;
+        int low = 0, high = nums.size() - 1;
+        int partnerIndex, i;
+        
+        while(low < high)
+        {
+            mid = low + (high - low)/2;
+            partnerIndex = mid ^ 1;
+            
+            if(nums[mid] == nums[partnerIndex])
+                low = mid + 1;
+            else
+                high = mid;
+        }
+        
+        return nums[low];
+    }
+};
