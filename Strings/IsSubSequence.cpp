9d939ec4a92f7e75ab7ec0cd6466d59145f59913
chitranshp <chitransh37@gmail.com>
2022-11-04 23:58:31 +0530

    Create IsSubSequence.cpp

---
+++ Strings/IsSubSequence.cpp
@@ -0,0 +1,15 @@
+//https://leetcode.com/problems/is-subsequence/
+
+class Solution {
+public:
+    bool isSubsequence(string s, string t) 
+    {
+        int i = 0, j = 0;
+
+        for(i = 0, j = 0; i < s.size() && j < t.size(); j++)
+            if(s[i] == t[j])
+                i++;
+
+        return i == s.size();
+    }
+};
