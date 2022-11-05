f0ea94e3b79137fab22346f1b4a369729a704759
chitranshp <chitransh37@gmail.com>
2022-11-05 19:09:37 +0530

    Update SingleNumber.cpp

--- Array/SingleNumber.cpp
+++ Array/SingleNumber.cpp
@@ -1,5 +1,24 @@
+// https://leetcode.com/problems/single-number/
+// O(n) TC and O(1) SC
+
+
 class Solution {
 public:
+    int singleNumber(vector<int>& nums) 
+    {
+        int ans = nums[0];
+
+        for(int i = 1; i < nums.size(); i++)
+            ans ^= nums[i];
+
+        return ans;
+
+    }
+};
+
+
+// O(n) SC
+/*
     int singleNumber(vector<int>& nums) 
     {
         unordered_map<int, int> m;
@@ -18,4 +37,5 @@ public:
         return ans;
         
     }
-};
+
+*/
