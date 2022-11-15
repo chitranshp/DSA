138258dc4ad6d18351ff790b15e1582bdc870e92
chitranshp <chitransh37@gmail.com>
2022-11-15 22:01:54 +0530

    Update file

--- Recursion/Subsets2.cpp
+++ Recursion/Subsets2.cpp
@@ -3,6 +3,7 @@
 
 //Naive version with extra DS(set)
 
+/*
 class Solution {
 public:
     void subsetHelper(vector<int> &nums, set<vector<int>> &res, int index, vector<int> temp)
@@ -31,3 +32,33 @@ public:
         return ans;
     }
 };
+*/
+
+//Optimised Version
+
+class Solution {
+public:
+    void subsetHelper(vector<int> &nums, vector<vector<int>> &res, int index, vector<int> temp)
+    {
+        res.push_back(temp);    //[],[1], [1, 2]
+
+        for(int i = index; i < nums.size(); i++)
+        {
+            if(i != index && nums[i] == nums[i - 1])
+                continue;
+            temp.push_back(nums[i]);
+            subsetHelper(nums, res, i + 1, temp);
+            temp.pop_back();
+        }
+    }
+
+    vector<vector<int>> subsetsWithDup(vector<int>& nums) 
+    {
+        vector<int> temp;
+        sort(nums.begin(), nums.end());
+        vector<vector<int>> res;
+        subsetHelper(nums, res, 0, temp);
+        return res;
+    }
+};
+
