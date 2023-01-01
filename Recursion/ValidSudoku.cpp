d1ee20cfa4e6e63ff640bf119cf109d037a5d09d
chitranshp <chitransh37@gmail.com>
2023-01-01 19:45:16 +0530

    Update file

--- Recursion/ValidSudoku.cpp
+++ Recursion/ValidSudoku.cpp
@@ -1,5 +1,40 @@
 // https://leetcode.com/problems/valid-sudoku/description/
 
+//Optimised version using hashing
+class Solution {
+public:
+    bool isValidSudoku(vector<vector<char>>& board)
+    {
+        int rows[9][9] = {0};
+        int cols[9][9] = {0};
+        int blocks[9][9] = {0};
+
+        for(int i = 0; i < board.size(); i++)
+        {
+            for(int j = 0; j < board[0].size(); j++)
+            {
+                if(board[i][j] != '.') 
+                {
+                    int c = board[i][j] - '1';          // 1 - '1' will be 0 and 9 - '1' will be 8.
+                    int k = 3 * (i / 3) + j / 3;        // 3 * (i / 3) gives us vertical partition and 3 * (i / 3) + j / 3 takes us to the correct sub-block by adding horizontal column offset
+                    
+                    if(rows[i][c] || cols[j][c] || blocks[k][c])
+                        return false;
+
+                    rows[i][c] = 1;
+                    cols[j][c] = 1;
+                    blocks[k][c] = 1;
+                }
+            }
+        }
+
+        return true;
+    }
+};
+
+
+
+/*
 class Solution {
 public:
     bool isValidSudoku(vector<vector<char>>& board, int row = 0, int col = 0) 
@@ -34,3 +69,4 @@ public:
         return isValidSudoku(board, row, col + 1);
     }
 };
+*/
