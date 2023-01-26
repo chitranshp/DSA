f8abacfd92555f67b763e6d0e7af9f655636e96a
chitranshp <chitransh37@gmail.com>
2023-01-27 00:00:02 +0530

    Update file

--- Recursion/RatInMaze.cpp
+++ Recursion/RatInMaze.cpp
@@ -5,25 +5,25 @@
 // TC O(4^(n*n)) SC O(n * n)
 class Solution{
     public:
-    bool dfs(vector<vector<int>> &m, int row, int col, string path, vector<string>& allpaths, int& n)
+    void dfs(vector<vector<int>> &m, int row, int col, string path, vector<string>& allpaths, int& n)
     {
         if(row == n - 1 && col == n - 1 && m[row][col] == 1)
         {
             allpaths.push_back(path);
-            return true;
+            return;
         }
         
         if(row < 0 || col < 0 || row >= n || col >= n || m[row][col] <= 0)
-            return false;
+            return;
         
         m[row][col] = m[row][col] * -1;
-        bool down = dfs(m, row + 1, col, path + 'D', allpaths, n);
-        bool left = dfs(m, row, col - 1, path + 'L', allpaths, n);
-        bool right = dfs(m, row, col + 1, path + 'R', allpaths, n);
-        bool up = dfs(m, row - 1, col, path + 'U', allpaths, n);
+        dfs(m, row + 1, col, path + 'D', allpaths, n);
+        dfs(m, row, col - 1, path + 'L', allpaths, n);
+        dfs(m, row, col + 1, path + 'R', allpaths, n);
+        dfs(m, row - 1, col, path + 'U', allpaths, n);
         m[row][col] = m[row][col] * -1;
         
-        return up || down || left || right;
+        return;
     }
     
     vector<string> findPath(vector<vector<int>> &m, int n) 
