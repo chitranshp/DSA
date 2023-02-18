08df96f3dcc7879cb1750bc7107db893d68cd8ee
chitranshp <chitransh37@gmail.com>
2023-02-18 23:57:38 +0530

    Update and rename NInjasTraining.cpp to NinjasTraining.cpp

---
+++ DP/NinjasTraining.cpp
@@ -0,0 +1,74 @@
+// https://www.codingninjas.com/codestudio/problems/ninja-s-training_3621003
+// https://practice.geeksforgeeks.org/problems/geeks-training/1
+
+/* 
+Recursion + Backtracking 
+Here as per question number of activity is fixed as 3. Only number of days(n) will be changing
+TC - o(3 ^ n) SC - O(3) (There will be n day's and on each day we will explore all possible ways i.e try to perform all valid activity combinations where activity is fixed as 3. Therefore 3 * 3 * 3 * 3 ....n times)
+*/
+
+class Solution {
+  public:
+    void helper(vector<vector<int>>& points, int day, int lastactivity, int currpoints, int& maxpoints)
+    {
+        if(day <= -1)
+        {
+            maxpoints = max(currpoints, maxpoints);
+            return;
+        }
+            
+        for(int activity = 0; activity < points[0].size(); activity++)
+        {
+            if(activity != lastactivity)
+            {
+                currpoints += points[day][activity];
+                helper(points, day - 1, activity, currpoints, maxpoints);
+                currpoints -= points[day][activity];
+            }
+        }
+    }
+    int maximumPoints(vector<vector<int>>& points, int n) 
+    {
+        int sum = 0, globalsum = INT_MIN;
+        helper(points, n - 1, -1, sum, globalsum);    //lastactivity as -1 indicates any valid activity can be taken for present day.
+        return globalsum;
+    }
+};
+
+/* 
+Approach 2: Using Plain Recursion
+Simplified the problem as just f(day, lastactivity). At each step, for given day and lastactivity performed we will try all possible valid activities and return the activity which gives maximum possible points for that step with the given last activity.
+                                                                    f(2, 3)   day = 2, last = 3(Which means 0, 1, 2 all will be valid for next step)
+                      points[1][0]  +  f(1, 0)                  points[1][1]+  f(1, 1)            points[1][2]  f(1, 2)       Note: here f(1, 0) means on day 1 activity 0 is performed. Therefore on next call i.e. for day 0 activity 0 cannot be performed
+                       f(0, 1)  f(0, 2)                          f(0, 0)   f(0, 2)       f(0, 0)   f(0, 1)
+                         0         0                                 0         0             0         0                       
+*/
+
+class Solution {
+  public:
+    int helper(vector<vector<int>>& points, int day, int lastactivity)
+    {
+        if(day < 0)
+            return 0;
+        
+        int maxpoints = INT_MIN;
+        for(int activity = 0; activity < points[0].size(); activity++)
+        {
+            if(activity != lastactivity)
+            {
+                int currpoints = points[day][activity] + helper(points, day - 1, activity);
+                maxpoints = max(maxpoints, currpoints);
+            }
+        }
+        return maxpoints;
+    }
+    
+    int maximumPoints(vector<vector<int>>& points, int n) 
+    {
+        int sum = 0, globalsum = INT_MIN;
+        vector<vector<int>> dp(n, vector<int>(4, -1)) ;
+            
+        //lastactivity as 3 indicates any valid activity can be taken for present day.
+        return helper(points, n - 1, 3);
+    }
+};
