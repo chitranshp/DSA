63f226141644b1dc5326e730f773e3b8d8e22d02
chitranshp <chitransh37@gmail.com>
2023-02-19 15:59:43 +0530

    Add memoization

--- DP/UniquePaths.cpp
+++ DP/UniquePaths.cpp
@@ -25,3 +25,34 @@ public:
         return helper(m, n, m - 1, n - 1);
     }
 };
+
+/* 
+Memoization - We observe that this problem has a lot of overlapping subproblems. So, using memoization to store results of repeating subproblems and reusing them
+We will use a vector of vector of size (n * m) for this.
+Total time complexity will be equal to the number of states ie O(n * m) as we will call f(0, 0), f(0, 1) to f(m - 1, n - 1) 
+Space complexity will be O(n * m) + O(n + m) 
+*/
+
+class Solution {
+public:
+    int helper(int m, int n, int r, int c, vector<vector<int>> &dp)
+    {
+        if(r == 0 && c == 0)
+            return 1;
+        else if(r > m - 1 || c > n - 1 || r < 0 || c < 0)
+            return 0;
+
+        if(dp[r][c] != -1)
+            return dp[r][c];
+
+        int up = helper(m, n, r - 1, c, dp);
+        int left = helper(m, n, r, c - 1, dp);
+
+        return dp[r][c] = up + left;
+    }
+    int uniquePaths(int m, int n) 
+    {
+        vector<vector<int>> dp(m, vector<int>(n, -1));
+        return helper(m, n, m - 1, n - 1, dp);
+    }
+};
