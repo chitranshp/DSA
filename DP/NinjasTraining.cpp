59ca6ab470076c2012555ce16cb42cba023d6369
chitranshp <chitransh37@gmail.com>
2023-02-19 15:18:16 +0530

    Update NinjasTraining.cpp

--- DP/NinjasTraining.cpp
+++ DP/NinjasTraining.cpp
@@ -77,6 +77,8 @@ class Solution {
 Memoization - TopDown DP
 TC - O(n * 4 * 3) SC - O(n) + O(n * 4)      
 There are N*4 states and for every state, we are running a for loop iterating three times.
+For each day there will be 4 states a 0,1,2,3 where 1 indicates task 1 was last performed and similarly task 3 indicates none of the tasks were performed on previous day.
+And for each state we will run a for loop from 0 -> 2 ie 3 iterations.
 */
 
 class Solution {
@@ -129,20 +131,81 @@ int maximumPoints(vector<vector<int>>& points, int n)
         dp[0][2] = max(points[0][0], points[0][1]);
         dp[0][3] = max(points[0][0], max(points[0][1], points[0][2]));  //3rd index will collect maximum of all points by all valid activity combinations till that day 
 				
-				for(day = 1; day < n; day++)
-					for(last = 0; last < 4; last++)
-					{
-						dp[day][last] = 0;
+	for(day = 1; day < n; day++)
+		for(last = 0; last < 4; last++)
+		{
+			dp[day][last] = 0;
         		for(int activity = 0; activity < points[0].size(); activity++)
         		{
-            	if(activity != last)
-            	{
-                int currpoints = points[day][activity] + dp[day - 1][activity];
-                dp[day][last] = max(dp[day][last], currpoints);
-           		}
+            			if(activity != last)
+            			{
+                			int currpoints = points[day][activity] + dp[day - 1][activity];
+                			dp[day][last] = max(dp[day][last], currpoints);
+					//dp[day][last] = max(dp[day][last], points[day][activity] + dp[day - 1][activity]);
+           			}
         		}
-					}
+		}
 		
         return dp[n - 1][4];
-    }				
-          
+}
+
+/*
+Space optimization 
+If we look closely at 
+	dp[day][last] = max(dp[day][las], points[day][activity] + dp[day - 1][activity]) 
+We are using only 2 rows of table dp at each step dp[day] and dp[day - 1]. We are using dp[day - 1] values to calculate the values for dp[day]. Therefore we don't need to store entire n * 4 table.
+Instead, we will store just one vector of size 1 * 4 and use that to find values for current day. After this we will store the new values over the existing previous one. And will make the new one as prev for next iteration.
+TC - O(n * 4 * 3) SC - O(4)
+*/
+
+class Solution {
+  public:
+    int helper(vector<vector<int>>& points, int day, int lastactivity, vector<vector<int>>& dp)
+    {
+        if(day < 0)
+            return 0;
+        
+        if(dp[day][lastactivity] != -1)
+            return dp[day][lastactivity];
+        
+        int maxpoints = INT_MIN;
+        for(int activity = 0; activity < points[0].size(); activity++)
+        {
+            if(activity != lastactivity)
+            {
+                int currpoints = points[day][activity] + helper(points, day - 1, activity, dp);
+                maxpoints = max(maxpoints, currpoints);
+            }
+        }
+        
+        return maxpoints;
+    }
+    
+    int maximumPoints(vector<vector<int>>& points, int n) 
+    {
+        vector<int> prev(4, 0);
+            
+        prev[0] = max(points[0][1], points[0][2]);
+        prev[1] = max(points[0][0], points[0][2]);
+        prev[2] = max(points[0][0], points[0][1]);
+        prev[3] = max(points[0][0], max(points[0][1], points[0][2]));  //3rd index will collect maximum of all points by all valid activity combinations till that day 
+            
+        for(int day = 1; day < n; day++)
+        {
+            vector<int> currdp(4, 0);       //To store the values for current day.
+            for(int last = 0; last < 4; last++)
+            {
+                for(int activity = 0; activity < 3; activity++)
+                {
+                    if(activity != last)
+                    {
+                        currdp[last] = max(currdp[last], points[day][activity] + prev[activity]);
+                    }
+                }
+            }
+            prev = currdp; //Make the current values as prev for the next iteration by overwriting it.
+        }
+            
+        return prev[3];
+    }
+};
