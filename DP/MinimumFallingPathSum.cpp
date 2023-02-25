8760c7d3b127a9bf983162e9eae0315cd0b842b7
chitranshp <chitransh37@gmail.com>
2023-02-25 19:16:16 +0530

    Update MinimumFallingPathSum.cpp

--- DP/MinimumFallingPathSum.cpp
+++ DP/MinimumFallingPathSum.cpp
@@ -1,7 +1,10 @@
 // https://leetcode.com/problems/minimum-falling-path-sum/description/
 
 /* 
-Recursion
+Recursion - There is no fixed starting single point and ending point is also variable. We will have to explore all n starting points to get our answer.
+For every point, we will explore 3 options(left, right, or top/bottom). And we will do that for all points along 1 column. Therefore time complexity will be around O(3 ^ n)
+Total time complexity = O(n(3 ^ n)) as we are calling the recursion n times.
+SC = O(n) 
 */
 
 class Solution {
@@ -35,6 +38,8 @@ public:
 
 /*
 Memoization - TopDown DP
+TC - O(n * n)
+SC - O(n * n) + O(n) 
 */
 
 class Solution {
@@ -65,6 +70,90 @@ public:
       
         for(int i = 0; i < matrix[0].size(); i++)
             res = min(res, helper(matrix, m - 1, i, dp));
+
+        return res;
+    }
+};
+
+
+/* 
+Tabulation - We are simply filling the table row by row.
+TC - O(n * n) + O(n) for finding the max/base case.
+SC - O(n * n) 
+*/
+
+class Solution {
+public:
+    int minFallingPathSum(vector<vector<int>>& matrix) 
+    {
+        int res = INT_MAX, m = matrix.size();
+        vector<vector<int>> dp(m, vector<int> (m, -1));
+        
+        //Base case
+        for(int c = 0; c < m; c++)
+            dp[0][c] = matrix[0][c];
+
+        for(int r = 1; r < m; r++)
+        {
+            for(int c = 0; c < m; c++)
+            {
+                int down = 1e9, left = 1e9, right = 1e9;
+                
+                down = matrix[r][c] + dp[r - 1][c];
+                if(c - 1 >= 0)
+                    left = matrix[r][c] + dp[r - 1][c - 1];
+
+                if(c + 1 <= m - 1)
+                    right = matrix[r][c] + dp[r - 1][c + 1];
+
+                dp[r][c] = min(left, min(down, right));
+            }
+        }
+
+        for(int c = 0; c < m; c++)
+            res = min(res, dp[m - 1][c]);
+
+        return res;
+    }
+};
+
+/*
+Space Optimization - We don't need the whole table at any point of time. We just need the prev column and we need another DS to store the computed values for current run.
+SC - O(N) + o(N)
+*/
+
+class Solution {
+public:
+
+    int minFallingPathSum(vector<vector<int>>& matrix) 
+    {
+        int res = INT_MAX, m = matrix.size();
+        vector<int> prev(m, 0), curr(m, 0);
+
+        for(int c = 0; c < m; c++)
+            prev[c] = matrix[0][c];
+
+        for(int r = 1; r < m; r++)
+        {
+            for(int c = 0; c < m; c++)
+            {
+                int down = 1e9, left = 1e9, right = 1e9;
+                
+                down = matrix[r][c] + prev[c];
+                if(c - 1 >= 0)
+                    left = matrix[r][c] + prev[c - 1];
+
+                if(c + 1 <= m - 1)
+                    right = matrix[r][c] + prev[c + 1];
+
+                curr[c] = min(left, min(down, right));
+            }
+
+            prev = curr;
+        }
+
+        for(int c = 0; c < m; c++)
+            res = min(res, prev[c]);
 
         return res;
     }
