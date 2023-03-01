065c66f613e116dabb5092cd9c3842098854bc2a
chitranshp <chitransh37@gmail.com>
2023-03-02 00:21:35 +0530

    Update SubsetSumEqualtoK.cpp

--- DP/SubsetSumEqualtoK.cpp
+++ DP/SubsetSumEqualtoK.cpp
@@ -41,3 +41,29 @@ Tabulation
 TC - O(N * k)
 Sc - O(N * k)
 */
+bool subsetSumToK(int n, int k, vector<int> &arr) 
+{
+    vector<vector<bool>> dp(n, vector<bool> (k + 1, false));
+
+    for(int i = 0; i < n; i++)
+    {
+        dp[i][0] = true;
+    }
+
+    dp[0][arr[0]] = true;
+
+    for(int i = 1; i < n; i++)
+    {
+        for(int target = 1; target <= k; target++)
+        {
+            bool notpick = dp[i - 1][target];
+            bool pick = false;
+            if(arr[i] <= target)
+                pick = dp[i - 1][target - arr[i]];
+
+            dp[i][target] = pick || notpick;
+        }
+    }
+
+    return dp[n - 1][k];
+}
