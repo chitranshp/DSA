3237ca99a9efee92dc2be8eeb5539ee581f206c3
chitranshp <chitransh37@gmail.com>
2023-03-03 02:35:19 +0530

    Update SubsetSumEqualtoK.cpp

--- DP/SubsetSumEqualtoK.cpp
+++ DP/SubsetSumEqualtoK.cpp
@@ -43,14 +43,18 @@ Sc - O(N * k)
 */
 bool subsetSumToK(int n, int k, vector<int> &arr) 
 {
+    // We want values from 0 to k. Therefore we declare a vector of size 'k + 1'.
     vector<vector<bool>> dp(n, vector<bool> (k + 1, false));
 
+    //Fills the first column
     for(int i = 0; i < n; i++)
     {
         dp[i][0] = true;
     }
 
-    dp[0][arr[0]] = true;
+    //Fills the first row, rest all are already set as false during initialization.
+    if(arr[0] <= k)
+        dp[0][arr[0]] = true;
 
     for(int i = 1; i < n; i++)
     {
@@ -65,7 +69,50 @@ bool subsetSumToK(int n, int k, vector<int> &arr)
         }
     }
 
+    // f(j, target) -> This signifies whether from index 0 to j, whether we can get a subset whose sum is equal to target
     return dp[n - 1][k];
 }
 
-/* ToDo Space optimal approach */
+/* 
+
+ToDo Space optimal approach 
+
+We see that to calculate a value of a cell of the dp array, we need only the previous row values (say prev). So, we don’t need to store an entire array. Hence we can space optimize it.
+Note: Whenever we create a new row ( say cur), we need to explicitly set its first element is true according to our base condition.
+
+TC - O(n * k)
+SC - O(k) + O(k)
+*/
+
+bool subsetSumToK(int n, int k, vector<int> &arr) 
+{
+    vector<bool> prev(k + 1, false), curr(k + 1, false);;
+    
+    //Base case
+    prev[0] = true;
+
+    if(arr[0] <= k)
+        prev[arr[0]] = true;
+    
+    for(int i = 1; i < n; i++)
+    {
+        //Set first column of each row as true
+        curr[0] = true;
+        for(int j = 1; j <= k; j++)
+        {
+            bool notpick = prev[j];
+            bool pick = false;
+            if(arr[i] <= j)
+                pick = prev[j - arr[i]];
+
+            curr[j] = pick || notpick;
+        }
+        
+        prev = curr;
+    }
+
+    // f(j, target) -> This signifies whether from index 0 to j, whether we can get a subset whose sum is equal to target.
+    return prev[k];
+}
+
+*/
