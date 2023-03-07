848b02b2c6a7b43e86d446d051c6d7082d32b494
chitranshp <chitransh37@gmail.com>
2023-03-07 23:53:54 +0530

    Create file

---
+++ DP/01 Knapsack.cpp
@@ -0,0 +1,81 @@
+// https://practice.geeksforgeeks.org/problems/0-1-knapsack-problem0945/1
+
+/*
+Recursion
+TC - O(2 ^ N)
+SC - O(N)
+*/
+
+class Solution
+{
+    public:
+    int helper(int target, int wt[], int val[], int index)
+    {
+        if(target == 0)
+            return 0;
+            
+        if(index == 0)
+        {
+            if(wt[0] <= target)
+                return val[0];       //Pick
+            else
+                return 0;           //NotPick
+        }
+        
+        int notpick = 0 + helper(target, wt, val, index - 1);
+        int pick = 0; 
+        if(wt[index] <= target)
+            pick = val[index] + helper(target - wt[index], wt, val, index - 1);
+            
+        return max(pick, notpick);
+    }
+
+    //Function to return max value that can be put in knapsack of capacity W.
+    int knapSack(int W, int wt[], int val[], int n) 
+    { 
+       return helper(W, wt, val, n - 1);
+    }
+};
+
+/* 
+dp[i][w] represents the max value(profit) that can be generated from index 0 to i, with current weight in bag being w(total)
+Memoization
+TC - O(N * W)
+SC - O(N * W) + O(N)
+*/
+class Solution
+{
+    public:
+    //Function to return max value that can be put in knapsack of capacity W.
+    int helper(int wt[], int val[], int target, int index, vector<vector<int>> &dp)
+    {
+        if(target == 0)
+            return 0;
+        // For last item, we will take the decision to pick or not pick based on the weight of that item and the remaining capacity of the bag.
+        if(index == 0)  //If we are at last item
+        {
+            //And if we still have space left in that bag such that last item can fit
+            if(wt[0] <= target)     //(Pick)
+                return val[0];      //Return the value of last item or include the last item
+            else                    //(Notpick) Dont include the last item, whatever value we have till now is the final value.
+                return 0;
+        }
+        
+        if(dp[index][target] != -1)
+            return dp[index][target];
+        
+        int notpick = 0 + (wt, val, target, index - 1, dp);
+        int pick = 0;
+        if(wt[index] <= target)
+            pick = val[index] + helper(wt, val, target - wt[index], index - 1, dp);
+            
+        return dp[index][target] = max(pick, notpick);
+    }
+
+    int knapSack(int W, int wt[], int val[], int n) 
+    { 
+       vector<vector<int>> dp(n, vector<int> (W + 1, -1));
+       return helper(wt, val, W, n - 1, dp);
+    }
+};
+
