c979958ee0fda4e24151f3458151e86e2a845215
chitranshp <chitransh37@gmail.com>
2023-03-14 23:56:49 +0530

    Update LCS.cpp

--- DP/LCS.cpp
+++ DP/LCS.cpp
@@ -1,31 +1,27 @@
 // https://leetcode.com/problems/longest-common-subsequence/description/
 
+/*
+Recursion
+TC - O()
+SC - O(m + n) Stack space for worst case
+*/
+
 class Solution {
 public:
-    int helperfun(string &text1, string &text2, int m, int n, int dp[][1001])
+    int helper(string& text1, string& text2, int m, int n)
     {
-        if(m == 0 || n == 0)
+        if(m < 0 || n < 0)
+        {
             return 0;
-
-        if(dp[m][n] != -1)
-            return dp[m][n];
-
-        if(text1[m - 1] == text2[n - 1])
-        {
-            dp[m][n] = 1 + helperfun(text1, text2, m - 1, n - 1, dp);
-            return dp[m][n];
-        }     
-        else
-        {
-            return dp[m][n] = max(helperfun(text1, text2, m - 1, n, dp), helperfun(text1, text2, m, n - 1, dp));
         }
-            
-    }
-    int longestCommonSubsequence(string text1, string text2)
-    {
-        int dp[1001][1001];
-        memset(dp, -1, sizeof(dp));
 
-        return helperfun(text1, text2, text1.size(), text2.size(), dp);
+        if(text1[m] == text2[n])
+            return 1 + helper(text1, text2, m - 1, n - 1);
+        else
+            return max(helper(text1, text2, m - 1, n), helper(text1, text2, m, n - 1));
+    }
+    int longestCommonSubsequence(string text1, string text2) 
+    {   
+        return helper(text1, text2, text1.size() - 1, text2.size() - 1);   
     }
 };
