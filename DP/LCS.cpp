00096babeb8d35d6e6a1ee7566c631057e32f8c3
chitranshp <chitransh37@gmail.com>
2023-03-15 03:11:17 +0530

    Todo Tabulation

--- DP/LCS.cpp
+++ DP/LCS.cpp
@@ -2,7 +2,7 @@
 
 /*
 Recursion
-TC - O()
+TC - Exponential.
 SC - O(m + n) Stack space for worst case
 */
 
@@ -25,3 +25,45 @@ public:
         return helper(text1, text2, text1.size() - 1, text2.size() - 1);   
     }
 };
+
+/*
+Memoization
+Here dp[i][j] represents the length of longest subsequence till index 'i' in string1 and index 'j' in string2.
+dp[i][j] = 0 if i < 0 or j < 0
+           1 if s1[i] is equal to s2[j]
+           max(f(i - 1, j), f(i, j - 1)) if s1[i] != s2[j]
+
+TC - O(m * n) 
+SC - O(m * n) + O(m + n)
+*/
+
+class Solution {
+public:
+    int helper(string& text1, string& text2, int m, int n, vector<vector<int>> &dp)
+    {
+        if(m < 0 || n < 0)
+        {
+            return 0;
+        }
+
+        if(dp[m][n] != -1)
+            return dp[m][n];
+
+        if(text1[m] == text2[n])
+            return dp[m][n] = 1 + helper(text1, text2, m - 1, n - 1, dp);
+        else
+            return dp[m][n] = max(helper(text1, text2, m - 1, n, dp), helper(text1, text2, m, n - 1, dp));
+    }
+    int longestCommonSubsequence(string text1, string text2) 
+    {   
+        vector<vector<int>> dp(string1.size(), vector<int> (string2.size(), -1));
+        return helper(text1, text2, text1.size() - 1, text2.size() - 1, dp);   
+    }
+};
+
+/*
+Tabulation
+TC - O(n * m)
+SC - O(n * m)
+*/
+
