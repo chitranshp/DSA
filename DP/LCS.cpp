37611fe9cf24efe7c4de26aece0a73547fb8a9bf
chitranshp <chitransh37@gmail.com>
2023-03-16 00:11:34 +0530

    Update files

--- DP/LCS.cpp
+++ DP/LCS.cpp
@@ -66,7 +66,7 @@ public:
 
 /*
 Tabulation - Our base case was if(i < 0 || j < 0) which can also be written as i == -1 || j == -1.
-To include case where i or j are -1, we will shift the index by one towards right. This means each index 'i' represents i - 1 in 0 - based indexing.
+To include case where i or j are -1, we will shift the index for dp(table) by one towards right. This means that each index 'i' represents i - 1 character of text1 and text2 in 0 - based indexing.
 
 New base case will be 
 if(i == 0 || j == 0)
@@ -88,17 +88,57 @@ public:
         for(int j = 0; j <= n; j++)
             dp[0][j] = 0;
 
+        //It is shifted by 1 place. Hence, we will loop till m and n instead of m - 1 and n - 1.
         for(int i = 1; i <= m; i++)
         {
             for(int j = 1; j <= n; j++)
             {
-                if(text1[i] == text2[j])
+                //Since i and j are looping over dp which is shifted by 1. We are decrementing them by 1 to compare for the actual current character in input strings.
+                //For i = 1 and j = 1, we will be compairing s1[0] and s2[0] and will do till s1[m - 1] and s2[n - 1]
+                if(text1[i - 1] == text2[j - 1])
                     dp[i][j] = 1 + dp[i - 1][j - 1];
                 else
                     dp[i][j] = 0 + max(dp[i - 1][j], dp[i][j - 1]);
             }
-        }
 
         return dp[m][n];   
     }
 };
+
+/*
+SpaceOptimization
+SC - O(N) + O(N)
+*/
+
+class Solution {
+public:
+    int longestCommonSubsequence(string text1, string text2) 
+    {   
+       int m = text1.size(), n = text2.size();
+       vector<int> prev(n + 1, 0), curr(n + 1, 0);
+       
+       //Next two loops can be skipped when it is initially initialized by 0.
+        for(int i = 0; i <= m; i++)
+            prev[0] = 0;
+
+        for(int j = 0; j <= n; j++)
+            prev[j] = 0;
+
+        for(int i = 1; i <= m; i++)
+        {
+            for(int j = 1; j <= n; j++)
+            {
+                if(text1[i - 1] == text2[j - 1])
+                    curr[j] = 1 + prev[j - 1];
+                else
+                    curr[j] = 0 + max(prev[j], curr[j - 1]);
+            }
+
+            prev = curr;
+        }
+
+        return prev[n];    
+    }
+};
+
+//Further SpaceOptimization is not possible as prev[j - 1] will be overwritten.
