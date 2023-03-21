4d8ee460effbf5dc68f7dd357fd8d06df80f1b9d
chitranshp <chitransh37@gmail.com>
2023-03-22 00:02:01 +0530

    Update files

--- DP/LongestSubstring.cpp
+++ DP/LongestSubstring.cpp
@@ -28,7 +28,7 @@ class Solution{
                 if(s1[i - 1] == s2[j - 1])
                 {
                     dp[i][j] = dp[i - 1][j - 1] + 1;
-                    maxi = max(dp[i][j], maxi);
+                    maxi = max(dp[i][j], maxi);         //To keep track of maximum length.
                 }
                 else
                     dp[i][j] = 0;
