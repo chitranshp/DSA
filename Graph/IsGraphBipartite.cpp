cf136f1da0fb5ab6822fe11bd2e4767ccba310b1
chitranshp <chitransh37@gmail.com>
2023-08-03 18:56:20 +0530

    Add approach 2 DFS

--- Graph/IsGraphBipartite.cpp
+++ Graph/IsGraphBipartite.cpp
@@ -4,7 +4,7 @@
 A graph is bipartite if the nodes can be partitioned into two independent sets A and B such that every edge in the graph connects a node in set A and a node in set B.
 Note:
 1. A bipartite graph is 2-colorable(Chromatic number <= 2).
-2. A graph containing odd length cycle is not bipartite.  (Even length cycle does not necessarily mean bipartite)
+2. A graph containing odd length cycle is not bipartite.  
 */
 
 /*
@@ -49,9 +49,47 @@ public:
 
     bool isBipartite(vector<vector<int>>& graph) {
         int m = graph.size(), n = graph[0].size();
+        vector<int> color(m, -class Solution {
+public:
+    bool dfs(vector<vector<int>> &graph, vector<int> &color, int node, int toFill)
+    {
+        color[node] = toFill;
+
+        for(int v: graph[node])
+        {
+            if(color[v] == -1)
+            {
+                if(dfs(graph, color, v, !toFill) == false)
+                    return false;
+            }
+            else
+            {
+                if(color[v] == toFill)
+                    return false;
+            }
+        }
+
+        return true;
+    }
+
+    bool isBipartite(vector<vector<int>>& graph) {
+        int m = graph.size(), n = graph[0].size();
+        int toFill = 0;
         vector<int> color(m, -1);
 
-        // It is not given that the graph is connected.
+        // In case there are multiple connected components
+        for(int i = 0; i < m; i++)
+        {
+            if(color[i] == -1)
+                if(dfs(graph, color, i, toFill) == false)
+                    return false;
+        }
+
+        return true;
+    }
+};1);
+
+        // It is not given that the graph is connected, there can be multiple components
         for(int i = 0; i < m; i++)      
         {
             if(color[i] == -1)          // This step is important. We won't try to fill a vertex with color if it is already filled in previous iteration.
@@ -62,3 +100,14 @@ public:
         return true;
     }
 };
+
+/*
+Approach2: Using DFS
+Pick a source node and recursively color all nodes connected to it. Repeat untill all nodes are colored or we find a node which is breaking the property.
+
+In DFS, we will carry forward the color that we want to fill the future node with. Inside dfs, if a adjacent node is not colored , we will recursively color it with the color which is not used by current node. But if it's already colored, we will just check whether it satisfies the property or not ie color is not same as the current node.
+
+TC - O(V + 2E)
+SC - O(2V) (Color + Recursion stack)
+*/
+
