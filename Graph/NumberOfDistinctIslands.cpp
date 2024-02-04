1fac12d8d3bedcbbddc45750f421f03a2cb4d1f5
chitranshp <chitransh37@gmail.com>
2024-02-04 15:49:37 +0530

    Update NumberOfDistinctIslands.cpp

--- Graph/NumberOfDistinctIslands.cpp
+++ Graph/NumberOfDistinctIslands.cpp
@@ -74,7 +74,7 @@ class Solution {
     int countDistinctIslands(vector<vector<int>>& grid) 
     {
         int m = grid.size(), n = grid[0].size();
-        unordered_set<vector<int>> unqIslands;
+        set<vector<int>> unqIslands;
         vector<vector<int>> vis(m, vector<int>(n, 0));
         
         for(int i = 0; i < m; i++)
