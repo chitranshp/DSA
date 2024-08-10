2ae9e7cb0a9936533dad674760aa2ebb57785197
chitranshp <chitransh37@gmail.com>
2024-08-11 03:47:37 +0530

    Update NumberOfProvinces.cpp

--- Graph/NumberOfProvinces.cpp
+++ Graph/NumberOfProvinces.cpp
@@ -97,3 +97,116 @@ public:
 };
 
 
+/*
+Approach 3: Using Disjoint set
+TC - O(n * n * α(n)) inverse Ackermann function, which grows very slowly and is almost constant for all practical purposes. Can be taken as O(n * n)
+SC - O(n)
+
+Note:
+Union by Rank is generally preferred when tree height is a key concern and when the problem involves 
+operations that benefit from managing TREE HEIGHT directly.
+Union by Size can be simpler and effective, particularly in cases where managing the size of each set is
+more intuitive or when the problem naturally fits a size-based approach.
+
+Mostly, we can pick either of two, based on ease of implementation. As time complexity is almost same for both in all practical purposes.
+*/
+
+class Solution {
+    vector<int> parent;
+    vector<int> rank;
+    vector<int> size;
+    
+public:
+    int findCircleNum(vector<vector<int>>& isConnected) {
+        int n = isConnected.size(), numProvinces = 0;
+        initializeDisjointSet(n);
+
+        for(int i = 0; i < n; i++)
+        {
+            // Checking right upper triangle only as matrix is symmetric
+            // If (2, 4) is covered that means edge (4, 2) is also covered. We can avoid redundant operatons.
+            for(int j = i + 1; j < n; j++)   
+            {
+                if(isConnected[i][j] == 1)
+                    unionBySize(i, j);
+            }
+        }
+
+        // We will calculate how many ultimate parents are there. And that is our number of provinces.
+        // Rest all nodes are part of any of these provinces
+        for(int i = 0; i < n; i++)
+        {
+            if(parent[i] == i)
+                numProvinces++;
+        }
+
+        return numProvinces;
+    }
+
+    int findParent(int node)
+    {
+        if(parent[node] == node)
+            return node;
+        return parent[node] = findParent(parent[node]);     // Path compression
+    }
+
+    // Performed on roots, not on individual nodes. So rank of Ultimate parents or roots might be updated
+    void unionByRank(int u, int v)
+    {
+        int rootU = findParent(u), rootV = findParent(v);
+        if(rootU == rootV)
+            return;
+        
+        // As we are adding tree with less rank to tree with higher rank, overall tree rank will remain
+        // unchanged and will still be equal to rank of tree having higher rank, in below case rank[rootV]
+        if(rank[rootU] < rank[rootV])
+        {
+            parent[rootU] = rootV;
+        }
+        else if(rank[rootU] > rank[rootV])
+        {
+            parent[rootV] = rootU;
+        }
+        else if(rank[rootU] == rank[rootV]) // As rank of both are equal, rank of combined tree will increase by 1
+        {
+            parent[rootV] = rootU;
+            rank[rootU] += 1;
+        }
+
+        return;
+    }
+
+    // Size remains unaffected by path compression, as the number of nodes for a root remains unchanged
+    // Union is done on roots, so rank and size will be considered of roots
+    void unionBySize(int u, int v)
+    {
+        int pu = findParent(u);
+        int pv = findParent(v);
+
+        if(pu == pv)
+            return;
+        else if(size[pu] < size[pv])
+        {
+            parent[pu] = pv;
+            size[pv] += size[pu];
+        }
+        else
+        {
+            parent[pv] = pu;
+            size[pu] += size[pv];
+        }
+        
+        return;
+    }
+
+    void initializeDisjointSet(int n) 
+    {
+        rank.resize(n, 0);
+        size.resize(n, 0);
+        parent.resize(n);
+        for(int i = 0; i < n; i++)
+            parent[i] = i;
+
+        return;
+    }
+};
