0f50f732865eb4b08bc191961add063187c28a79
chitranshp <chitransh37@gmail.com>
2024-08-12 02:17:51 +0530

    Update NoOfOperationsToConnect.cpp

--- Graph/NoOfOperationsToConnect.cpp
+++ Graph/NoOfOperationsToConnect.cpp
@@ -3,7 +3,7 @@
 /*
 If there are n components in a graph, we will require n - 1 edges to connect all of them and make a single connected component or connected graph.
 Steps:
-1. Find number of connected Components.
+1. Find number of connected Components. (Disjoint set or DFS/BFS can be used)
 2. Find number of extra edges so that we know how many edges we can remove and use it elsewhere.
 3. Check if extra edges >= required edges to make graph connected.
 
