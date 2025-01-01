da2242bdf67555b4560dae1928f805e5e0fcde9f
chitranshp <chitransh37@gmail.com>
2025-01-01 14:03:58 +0530

    Create MaximumScoreAfterSplittingString.cpp

---
+++ Strings/MaximumScoreAfterSplittingString.cpp
@@ -0,0 +1,43 @@
+// https://leetcode.com/problems/maximum-score-after-splitting-a-string/
+
+/*
+Approach 1: Two pass version
+TC - O(n)
+SC - O(1)
+*/
+
+class Solution {
+public:
+    int maxScore(string s) 
+    {
+        int totalZeros = 0, totalOnes = 0;
+        for(const char &ch: s)
+        {
+            if(ch == '0') totalZeros++;
+        }
+
+        totalOnes = s.size() - totalZeros;
+
+        if(totalZeros == 0)             // All characters are either all zeroes or all ones.
+            return s.size() - 1;
+
+        int maxScore = 0, leftZeros = 0, rightOnes = totalOnes;
+
+        // Traverse upto second last split to consider all valid splits. Split with empty left or right subsplit is invalid
+        for(int i = 0; i < s.size() - 1; i++)
+        {
+            if(s[i] == '0')
+            {
+                leftZeros++;
+            }
+            else
+            {
+                rightOnes--;
+            }
+
+            maxScore = max(maxScore, leftZeros + rightOnes);
+        }
+
+        return maxScore;
+    }
+};
