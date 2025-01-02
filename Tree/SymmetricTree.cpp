8d50f6af883479caf1b5e3c4a71b5120655111b1
chitranshp <chitransh37@gmail.com>
2025-01-02 21:18:18 +0530

    Update SymmetricTree.cpp

--- Tree/SymmetricTree.cpp
+++ Tree/SymmetricTree.cpp
@@ -1,16 +1,9 @@
 //https://leetcode.com/problems/symmetric-tree/
 
-/**
- * Definition for a binary tree node.
- * struct TreeNode {
- *     int val;
- *     TreeNode *left;
- *     TreeNode *right;
- *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
- *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
- *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
- * };
- */
+/*
+TC - O(n)
+SC - O(h)
+*/
 class Solution {
 public:
     bool checkSymmetric(TreeNode* root1, TreeNode* root2)
