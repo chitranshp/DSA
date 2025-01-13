ef05b7cd1876a4d3ce9d42d09f38b4fd25b34312
chitranshp <chitransh37@gmail.com>
2025-01-14 02:38:56 +0530

    Update CeilInBST.cpp

--- Tree/CeilInBST.cpp
+++ Tree/CeilInBST.cpp
@@ -9,6 +9,35 @@ int findCeil(Node* root, int input) {
     if (root == NULL) return -1;
     
     Node* curr = root;
+    int ceil = -1;                  // Default initialization for the case when ceil is not found. All values in tree are lesser then input
+    while(curr != nullptr)
+    {
+        if(curr->data == input)
+        {
+          ceil = curr->data;
+          break;
+        }
+        else if(curr->data > input)
+        {
+            // We are going down in the left subtree as seen in next step. So, all further values for curr are bound to have lesser value then curr->data
+            // At the same time will be greater than input because of else if condition
+            ceil = curr->data;        
+            curr = curr->left;
+        }
+        else 
+        {
+            curr = curr->right;
+        }
+    }
+        
+    return ceil;
+}
+
+// Approach 2: Similar with redundant checks
+int findCeil(Node* root, int input) {
+    if (root == NULL) return -1;
+    
+    Node* curr = root;
     int ceil = INT_MAX;
     while(curr != nullptr)
     {
@@ -26,32 +55,4 @@ int findCeil(Node* root, int input) {
     }
         
     return (ceil == INT_MAX)? -1: ceil;
-}
-
-// Approach 2: Same approach with removing redundant/unnecessary steps
-
-int findCeil(Node* root, int input) {
-    if (root == NULL) return -1;
-    
-    Node* curr = root;
-    int ceil = -1;                  // Default initialization for the case when ceil is not found. All values in tree are lesser then input
-    while(curr != nullptr)
-    {
-        if(curr->data == input)
-        {
-          ceil = curr->data;
-          break;
-        }
-        else if(curr->data > input)
-        {
-            ceil = curr->data;        // We are going down in the left subtree as seen in next step. So, all further values for curr are bound to have lesser value then curr->data
-            curr = curr->left;
-        }
-        else 
-        {
-            curr = curr->right;
-        }
-    }
-        
-    return ceil;
 }
